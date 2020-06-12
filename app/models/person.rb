c9cf185ec8c040c16d0d1d8bf3227a6d5cb849c7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-12 18:25:01 +0000

    Automatically backed up by Learn

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,6 @@
 class Person < ActiveRecord::Base
   has_many :addresses
   accepts_nested_attributes_for :addresses
-  #the above is how we access our nested hashes 
+  #the above is how we access our nested hashes. This form_for method also writes our writer (which creates our getter/setter methods) addresses_attributes 
 
 end
